d52a01b789318cd5ac19fbfc11a4f92ecae42c29
Philip Georgis <philip.georgis@gmail.com>
2026-09-14 10:42:12 +0200

    Move audio stream label field higher in GUI

--- recorder/gui/main_window.py
+++ recorder/gui/main_window.py
@@ -271,10 +271,10 @@ class MainWindow(QMainWindow):
 
         af.addRow(self.audio_enabled)
         af.addRow("Input device", self.audio_device)
+        af.addRow("Stream name", self.audio_stream_name)
         af.addRow("Sample rate", self.audio_sr)
         af.addRow("Bit depth", self.audio_bit)
         af.addRow("Channels", self.audio_ch)
-        af.addRow("Stream name", self.audio_stream_name)
         audio_widget.setLayout(af)
         self.tabs.addTab(audio_widget, "Audio")
 
